f7efa159feab4a92939374f941498e67ba96e93e
nikhil <nikhilpokuri143@gmail.com>
2024-12-09 19:21:36 +0530

    feed

--- marvel/static/js/style.js
+++ marvel/static/js/style.js
@@ -3,15 +3,11 @@ const childClass = document.getElementsByClassName("hero-div");
 for (let i = 0; i < hTags.length; i++) { //for every h1 tag
     const element = hTags[i];
     element.addEventListener("mouseover", function(){
-        for (let j = 0; j < childClass.length; j++) { //for every hero-div
-            const cdiv = childClass[j];
+            const cdiv = childClass[i-1];
             cdiv.style.backgroundColor = "rgba(187, 178, 178, 0.536)";
-        }
     })
     element.addEventListener("mouseleave", function(){
-        for (let j = 0; j < childClass.length; j++) {
-            const cdiv = childClass[j];
+            const cdiv = childClass[i-1];
             cdiv.style.backgroundColor = "";
-        }
     })
 }
